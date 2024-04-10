edd78980bbbde4a4d1ba778104aa11dea3748f2c
alvaro <alvaro.torres.chacon@colegiolitterator.com>
2024-04-10 16:01:05 +0200

    primer commit

---
+++ maquina_virtual/src/maquina_virtual/Memory.java
@@ -0,0 +1,15 @@
+package maquina_virtual;
+
+public class Memory {
+	private Integer[] memory;
+	private int MAX_MEMORY = 200;
+	private int size;
+	
+	public boolean write(int pos, int value) {
+		
+	}
+	
+	public Integer read(int pos) {
+		
+	}
+}
---
+++ maquina_virtual/src/maquina_virtual/OperandStack.java
@@ -0,0 +1,11 @@
+package maquina_virtual;
+
+public class OperandStack {
+	int MAX_STACK = 200;
+	private int [] STACK;
+	int numElements;
+	
+	public String toString() {
+		
+	}
+}
